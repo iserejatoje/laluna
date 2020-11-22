46ff16ae05d6f7fb8ecf4e48074a687adec0fe95
Sergey Valitov <serejatoje@gmail.com>
2020-11-22 23:31:11 +0700

    Новиночка

---
+++ app/js/app.js
@@ -0,0 +1,6 @@
+// new fullpage('#fullpage', {
+//     navigation: true,
+//     navigationPosition: 'left',
+//     navigationTooltips: ['Доходы', 'Доходы'],
+//     showActiveTooltip: true,
+// });
