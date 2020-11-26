8757b23e4a0934a1f75dd83691ac2cb98ce9d37b
Sergey Valitov <serejatoje@gmail.com>
2020-11-26 18:25:02 +0700

    Фиксы

--- app/js/app.js
+++ app/js/app.js
@@ -19,8 +19,11 @@ function init() {
             $('[href="#money"]').addClass('active').siblings().removeClass('active');
         }
 
-        if ($(window).scrollTop() > $('#vacancy').offset().top - 180 && $(window).scrollTop() < $('#feedback').offset().top - 180) {
+        if ($(window).scrollTop() > $('#vacancy').offset().top - 200 && $(window).scrollTop() < $('#feedback').offset().top - 200) {
             $('[href="#vacancy"]').addClass('active').siblings().removeClass('active');
+        }
+        if ($(window).scrollTop() > $('#feedback').offset().top - 190) {
+            $('[href="#feedback"]').addClass('active').siblings().removeClass('active');
         }
     }
 }
@@ -30,12 +33,29 @@ let $transition = false;
 $(document).ready(function () {
    init();
 
+   $('[type="tel"]').mask('+7 (999) 999-99-99');
+
+   $('input').on('focus', function () {
+       if ($.trim($(this).val()) === '')
+           $(this).parent().addClass('active')
+   });
+
+   $('input').on('blur', function () {
+       if ($.trim($(this).val()) === '')
+           $(this).parent().removeClass('active')
+   });
+
    $('nav').on('click', 'a', function () {
        $transition = true;
        $('body, html').animate({scrollTop: $($(this).attr('href')).offset().top}, 600, function() {
            $transition = false;
        });
        $(this).addClass('active').siblings().removeClass('active');
+       return false;
+   })
+
+   $('.go').on('click', function () {
+       $('body, html').animate({scrollTop: $('#feedback').offset().top}, 1200);
        return false;
    })
 });
